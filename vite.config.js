8a624ac9600b584343ace9d2153567a448d5948b
Maya Shkolnic <mayashkolnic71@gmail.com>
2026-06-15 14:19:40 +0300

    Update vite config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        demo: resolve(__dirname, 'demo.html')
+      }
+    }
+  }
 })
